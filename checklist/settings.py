f3e8eae677f8f8fa296f69b1b779dc37964bc4c0
joao garcia <joaogarcialn@hotmail.com>
2018-05-30 00:08:10 -0300

    correcao flake8

--- checklist/settings.py
+++ checklist/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'checklist.core'
+    'checklist.core',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +120,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-

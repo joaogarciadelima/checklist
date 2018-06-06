a5146a7cbd0ac110324b9a15431918d779fb4021
joao garcia <joaogarcialn@hotmail.com>
2018-06-06 17:20:41 -0300

    Adicionado Flake8

---
+++ checklistos/core/tests/test_url.py
@@ -0,0 +1,5 @@
+from checklistos.urls import urlpatterns
+
+
+def test_urls_len():
+    assert 2 == len(urlpatterns)
--- checklistos/urls.py
+++ checklistos/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from checklistos.core.views import home
+
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
 ]

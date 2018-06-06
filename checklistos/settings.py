c87e99e2d2e656552654c520dc2407cd0dd814ad
joao garcia <joaogarcialn@hotmail.com>
2018-06-06 19:26:49 -0300

    Alterado template

--- checklistos/settings.py
+++ checklistos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUB', default=False, Cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 

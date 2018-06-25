b043eca90530b4f4d3b4f71a8ff54a0f70ef4d37
joao garcia <joaogarcialn@hotmail.com>
2018-06-25 19:40:07 -0300

    adicionada definição detalhada do projeto

--- checklistos/core/tests/tests.py
+++ checklistos/core/tests/tests.py
@@ -1,3 +1,17 @@
-def test_home(client):
+import pytest
+
+from checklistos.django_assertions import dj_assert_contains
+
+
+def test_status_code(client):
     response = client.get('/')
     assert 200 == response.status_code
+
+
+@pytest.mark.parametrize('content', [
+    'Checklist OS',
+    'Joao Garcia de Lima Neto'
+])
+def test_home(client, content):
+    response = client.get('/')
+    dj_assert_contains(response, content)
